78ccfecd5b7afdc51f5ab1f3f1d5d94e965ee2a3
juan <juano.02@hotmail.com>
2021-12-28 17:20:40 +0100

    run video script on dom load

--- assets/script.js
+++ assets/script.js
@@ -6,27 +6,29 @@ if (document.getElementsByClassName("start-logo-title-element").length != 0) {
 	});
 }
 
-if (document.getElementsByClassName("cover-video").length != 0) {
-	let currentVideo = Math.floor(Math.random() * 3);
-	let prevVideo = Math.floor(Math.random() * 3);
-	let coverVideos = document.getElementsByClassName("cover-video");
-	randomVideo();
+window.addEventListener("DOMContentLoaded", () => {
+	if (document.getElementsByClassName("cover-video").length != 0) {
+		let currentVideo = Math.floor(Math.random() * 3);
+		let prevVideo = Math.floor(Math.random() * 3);
+		let coverVideos = document.getElementsByClassName("cover-video");
+		randomVideo();
 
-	function randomVideo() {
-		while (currentVideo == prevVideo) {
-			currentVideo = Math.floor(Math.random() * 3);
-		}
-		if (coverVideos[prevVideo] != null) {
-			coverVideos[prevVideo].classList.remove("cover-video-visible");
-		}
-		prevVideo = currentVideo;
-		coverVideos[currentVideo].classList.add("cover-video-visible");
+		function randomVideo() {
+			while (currentVideo == prevVideo) {
+				currentVideo = Math.floor(Math.random() * 3);
+			}
+			if (coverVideos[prevVideo] != null) {
+				coverVideos[prevVideo].classList.remove("cover-video-visible");
+			}
+			prevVideo = currentVideo;
+			coverVideos[currentVideo].classList.add("cover-video-visible");
 
-		setTimeout(function () {
-			randomVideo();
-		}, 5000);
+			setTimeout(function () {
+				randomVideo();
+			}, 5000);
+		}
 	}
-}
+});
 
 function displayMenu() {
 	let bottomMenu = document.getElementsByClassName("bottom-menu-wrapper");
