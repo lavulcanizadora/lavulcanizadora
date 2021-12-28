d295c621c1918def7a3ed1c86e32b3ba156fad55
juan <juano.02@hotmail.com>
2021-12-28 22:01:58 +0100

    unbuilt javascript start changes

--- _site/assets/script.js
+++ _site/assets/script.js
@@ -1,32 +1,33 @@
-if (document.getElementsByClassName("cover-video").length != 0) {
-	let currentVideo = Math.floor(Math.random() * 3);
-	let prevVideo = Math.floor(Math.random() * 3);
-	let coverVideos = document.getElementsByClassName("cover-video");
-	randomVideo();
-
-	function randomVideo() {
-		while (currentVideo == prevVideo) {
-			currentVideo = Math.floor(Math.random() * 3);
-		}
-		if (coverVideos[prevVideo] != null) {
-			coverVideos[prevVideo].classList.remove("cover-video-visible");
-		}
-		prevVideo = currentVideo;
-		coverVideos[currentVideo].classList.add("cover-video-visible");
-
-		setTimeout(function () {
-			randomVideo();
-		}, 5000);
-	}
-}
-
 if (document.getElementsByClassName("start-logo-title-element").length != 0) {
 	let logoTitle = document.getElementsByClassName("start-logo-title-element")[0];
 	logoTitle.addEventListener("click", displayMenu);
+	setTimeout(() => {
+		displayMenu(), 3000;
+	});
 }
 
-setTimeout(() => {
-	displayMenu(), 3000;
+window.addEventListener("DOMContentLoaded", () => {
+	if (document.getElementsByClassName("cover-video").length != 0) {
+		let currentVideo = Math.floor(Math.random() * 3);
+		let prevVideo = Math.floor(Math.random() * 3);
+		let coverVideos = document.getElementsByClassName("cover-video");
+		randomVideo();
+
+		function randomVideo() {
+			while (currentVideo == prevVideo) {
+				currentVideo = Math.floor(Math.random() * 3);
+			}
+			if (coverVideos[prevVideo] != null) {
+				coverVideos[prevVideo].classList.remove("cover-video-visible");
+			}
+			prevVideo = currentVideo;
+			coverVideos[currentVideo].classList.add("cover-video-visible");
+
+			setTimeout(function () {
+				randomVideo();
+			}, 5000);
+		}
+	}
 });
 
 function displayMenu() {
