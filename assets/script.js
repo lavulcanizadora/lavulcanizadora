c7fc9c0f8dba3cb3ed8f694de51ea9bb087f40ce
juan <juano.02@hotmail.com>
2021-12-29 12:33:37 +0100

    changed video load process

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,31 @@ if (document.getElementsByClassName("start-logo-title-element").length != 0) {
 	});
 }
 
+let videosLoaded = false;
+let videos = document.getElementsByClassName("cover-video");
+
+videos[0].addEventListener("loadeddata", (e) => {
+	loadVideoTwo();
+});
+
+const loadVideoTwo = () => {
+	let videos = document.getElementsByClassName("cover-video");
+	videos[1].src = "/uploads/loop2.mp4";
+	loadVideoThree();
+};
+
+const loadVideoThree = () => {
+	let videos = document.getElementsByClassName("cover-video");
+	videos[2].src = "/uploads/loop3.mp4";
+	videosLoaded = true;
+};
+
 window.addEventListener("DOMContentLoaded", () => {
 	if (document.getElementsByClassName("cover-video").length != 0) {
 		let currentVideo = Math.floor(Math.random() * 3);
 		let prevVideo = Math.floor(Math.random() * 3);
 		let coverVideos = document.getElementsByClassName("cover-video");
+
 		randomVideo();
 
 		function randomVideo() {
